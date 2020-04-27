c1c2e5aa437855e558b0affe81bf315bd9eb4969
shuichi <shu.heihei1209@gmail.com>
2020-04-27 14:34:11 +0900

    finish5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '58e1e02875661271f25d7451aa42e5fa6d7114de7665e58e71ae8e9fae3f41ed88b2a1df40fb9198e9ce642339ddb05647a1c2aeb84d1393fac81fd83d2f5a40'
